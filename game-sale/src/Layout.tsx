60d196a63e6f308fa09235caedc3c7c7eba083ed
rjl98 <rodolfo.j.lopez.h@gmail.com>
2024-07-01 16:27:34 -0400

    Game Sale

--- game-sale/src/Layout.tsx
+++ game-sale/src/Layout.tsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import LayoutProvider from "./providers/LayoutProvider";
 
 function Layout() {
-    document.title = "Game Sale";
     return (
         <LayoutProvider>
             <Outlet />
